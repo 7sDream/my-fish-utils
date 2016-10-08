282aae808581a74594c5bda393f7b7d49ec1cea5
7sDream <7seconddream@gmail.com>
2016-10-08 13:44:50 +0800

    [Change] script filename

---
+++ functions/pc.fish
@@ -0,0 +1,19 @@
+set -l cmd1 "proxychains4"
+set -l cmd2 "proxychains"
+
+if command -s "$cmd1" > /dev/null
+    set cmd $cmd1
+else if command -s "$cmd2" > /dev/null
+    set cmd $cmd2
+else
+    echo "Please install proxychains-ng to use pc and spc command."
+    echo "Github repo of proxychains-ng: https://github.com/rofl0r/proxychains-ng"
+end
+
+function pc -w "$cmd" -V cmd
+    eval "$cmd" $argv
+end
+
+function spc -w "$cmd" -V cmd
+    eval sudo "$cmd" $argv
+end
